270be879ff2285c8def1a635c7c1285139612cb7
levraimerwane@gmail.com <levraimerwane@gmail.com>
2026-04-28 18:47:52 +0200

    Fix the scan not working and added single port scan option

--- bin/nps.py
+++ bin/nps.py
@@ -2,20 +2,44 @@ import socket
 
 def run(options):
     target = options.get("target", None)
-    print(f"Scanning {target}...")
+    port = options.get("p")
 
-    for port in range(1, 1000):
-        scan_port(target, port)
+    if not target:
+        print("Enter a Target.")
+
+        return
+    print(f"Scanning {target}...")
     
-def scan_port(target, port):
+    if port:
+        port = int(port)
+        scan_port(target, port)
+    else:
+        for port in range(1, 8888):
+            scan_range(target, port)
+    
+def scan_range(target, port):
     try:
-        sock = socket.socket(socket.AF_INET, socket.SOCK_SEQPACKET)
-        sock.settimeout(15)
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.settimeout(1)
 
         result = sock.connect_ex((target, int(port)))
         if result == 0:
             print(f"Port {port} Open.")
 
         sock.close()
+    except Exception as e:
+        print(e)
+
+def scan_port(target, port):
+    try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.settimeout(1)
+
+        result = sock.connect_ex((target, int(port)))
+        if result == 0:
+            print(f"Port {port} Open.")
+        else:
+            sock.close()
+            print(f"Port {port} Closed.")
     except Exception as e:
         print(e)
