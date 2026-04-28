c741804f1205465bc324de2ef6cb470e8999c53a
levraimerwane@gmail.com <levraimerwane@gmail.com>
2026-04-28 18:19:05 +0200

    Added scan port function and scan range of ports for loop

--- bin/nps.py
+++ bin/nps.py
@@ -1,2 +1,21 @@
+import socket
+
 def run(options):
     target = options.get("target", None)
+    print(f"Scanning {target}...")
+
+    for port in range(1, 1000):
+        scan_port(target, port)
+    
+def scan_port(target, port):
+    try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_SEQPACKET)
+        sock.settimeout(15)
+
+        result = sock.connect_ex((target, int(port)))
+        if result == 0:
+            print(f"Port {port} Open.")
+
+        sock.close()
+    except Exception as e:
+        print(e)
